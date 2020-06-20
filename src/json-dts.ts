5cc8bd8c32fea8a9060f3c3b9824293f6ce82f1d
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2020-06-20 12:21:12 +0200

    refactor: Move script inside main()
    refactor: array warning

--- src/json-dts.ts
+++ src/json-dts.ts
@@ -26,6 +26,7 @@ const identifierRegex = /^[$_a-z][$_a-z0-9]*$/i;
  * Name chosen as such to hopefully avoid collision with an input file.
  **/
 const COMMON_FILE = "_common";
+const COMMON_DTS = COMMON_FILE + ".d.ts";
 
 const USAGE = "Usage: node json-dts.js INPUT-DIR OUTPUT-DIR";
 const INSTRUCTIONS = `
@@ -33,6 +34,21 @@ Reads all JSON files inside INPUT-DIR (including those in subdirectories),
 parses each into a TS declaration file with matching name and places those
 into OUTPUT-DIR, matching the folder structure inside of INPUT-DIR.
 `.trim();
+
+const UNKNOWN_ARRAY_WARNING = (declarations: TypeDeclaration[]) =>
+  `
+The proper array type for the following type aliases could not be
+inferred because the provided JSON featured empty arrays:
+
+${declarations
+  .map(
+    ({ id, file, context }) =>
+      `  type ${typeAlias(id)}, derived from ${file}:${context}`,
+  )
+  .join("\n")}
+
+These type aliases have been given the type "unknown[]". Opening
+${COMMON_DTS} and manually providing the proper types is recommended.`.trim();
 
 if (process.argv.length === 3 && ["-h", "--help"].includes(process.argv[2])) {
   console.log(USAGE + "\n\n" + INSTRUCTIONS);
@@ -92,6 +108,14 @@ function convertToType(x: JSONValue, file: string) {
   return _convertToType(x, "root");
 }
 
+/** A cache mapping hashes of types to the types themselves. */
+const cache = new Map<string, TypeDeclaration>();
+/** Increments the cache id on each call. */
+const cacheId = (() => {
+  let id = 0;
+  return () => id++;
+})();
+
 /**
  * Returns a type alias for the given type. Gives a brand new alias and adds
  * the type to the type cache if the type is new. Alternatively, it reuses
@@ -120,14 +144,6 @@ function createType(type: Type, file: string, context: string, unique = false) {
   return typeName;
 }
 
-/** A cache mapping hashes of types to the types themselves. */
-const cache = new Map<string, TypeDeclaration>();
-/** Increments the cache id on each call. */
-const cacheId = (() => {
-  let id = 0;
-  return () => id++;
-})();
-
 /** Generates a type alias from a given id. */
 function typeAlias(id: number) {
   return "T" + id;
@@ -153,81 +169,73 @@ function getTypeDeclaration(declaration: TypeDeclaration) {
   return `type ${typeName} = C<{ ${declarations} }>;`;
 }
 
-const inputFiles = glob.sync("**/*.json", { cwd: inputDir });
-const exportedTypes = new Set<string>();
-let currentFile = 1;
+function main() {
+  const inputFiles = glob.sync("**/*.json", { cwd: inputDir });
+  const exportedTypes = new Set<string>();
+  let currentFile = 1;
 
-console.log("Parsing JSON files...");
-for (const file of inputFiles) {
-  const json = readJSONSync(path.resolve(inputDir, file));
-  const typeName = convertToType(json, file);
-  exportedTypes.add(typeName);
+  console.log("Parsing JSON files...");
+  for (const file of inputFiles) {
+    const json = readJSONSync(path.resolve(inputDir, file));
+    const typeName = convertToType(json, file);
+    exportedTypes.add(typeName);
 
-  mkdirp.sync(path.resolve(outputDir, path.dirname(file)));
-  const outputFile = path.resolve(outputDir, file.replace(".json", ".d.ts"));
-  const relativePath = path.relative(path.dirname(outputFile), outputDir);
-  const relativeImport = relativePath
-    ? path.join(relativePath, COMMON_FILE)
-    : "./" + COMMON_FILE;
+    mkdirp.sync(path.resolve(outputDir, path.dirname(file)));
+    const outputFile = path.resolve(outputDir, file.replace(".json", ".d.ts"));
+    const relativePath = path.relative(path.dirname(outputFile), outputDir);
+    const relativeImport = relativePath
+      ? path.join(relativePath, COMMON_FILE)
+      : "./" + COMMON_FILE;
 
-  fs.writeFileSync(
-    outputFile,
-    `export { ${typeName} as default } from "${relativeImport}";`,
-  );
+    fs.writeFileSync(
+      outputFile,
+      `export { ${typeName} as default } from "${relativeImport}";`,
+    );
 
-  console.log(`Finished file ${currentFile} of ${inputFiles.length}: ${file}`);
-  currentFile += 1;
-}
-
-console.log(`Creating ${COMMON_FILE}.d.ts file...`);
-const output = fs.createWriteStream(
-  path.resolve(outputDir, COMMON_FILE + ".d.ts"),
-);
-
-// Include a Compute utility which makes resulting types easier to read
-// with Intellisense by expanding them fully, instead of leaving
-// object properties with cryptic type names.
-output.write(`type C<A extends any> = {[K in keyof A]: A[K]} & {};\n\n`);
-
-const unknownArrays = new Set<TypeDeclaration>();
-for (const declaration of cache.values()) {
-  if (declaration.type === "unknown[]") {
-    unknownArrays.add(declaration);
+    console.log(
+      `Finished file ${currentFile} of ${inputFiles.length}: ${file}`,
+    );
+    currentFile += 1;
   }
 
-  const typeDeclaration = getTypeDeclaration(declaration);
-  if (exportedTypes.has(typeAlias(declaration.id))) {
-    output.write("export ");
+  console.log(`Creating ${COMMON_DTS} file...`);
+  const output = fs.createWriteStream(path.resolve(outputDir, COMMON_DTS));
+
+  output.write(
+    `
+/**
+ * Compute utility which makes resulting types easier to read
+ * with IntelliSense by expanding them fully, instead of leaving
+ * object properties with cryptic type names.
+ */
+type C<A extends any> = {[K in keyof A]: A[K]} & {};
+`.trim(),
+  );
+  output.write("\n\n");
+
+  const unknownArrays: TypeDeclaration[] = [];
+  for (const declaration of cache.values()) {
+    if (declaration.type === "unknown[]") {
+      unknownArrays.push(declaration);
+    }
+
+    const typeDeclaration = getTypeDeclaration(declaration);
+    if (exportedTypes.has(typeAlias(declaration.id))) {
+      output.write("export ");
+    }
+
+    output.write(typeDeclaration);
+    output.write(` // ${declaration.file}:${declaration.context}\n`);
   }
 
-  output.write(typeDeclaration);
-  output.write(` // ${declaration.file}:${declaration.context}\n`);
+  output.close();
+  console.log(COMMON_DTS + " created successfully.");
+
+  if (unknownArrays.length) {
+    console.log("\n" + UNKNOWN_ARRAY_WARNING(unknownArrays));
+  }
 }
 
-output.close();
-console.log(COMMON_FILE + ".d.ts created successfully.");
-
-if (unknownArrays.size === 0) {
-  process.exit(0);
+if (!module.parent) {
+  main();
 }
-
-console.log();
-console.log(
-  `
-The proper array type for the following type aliases could not be
-inferred because the provided JSON featured empty arrays:
-`.trim(),
-);
-for (const declaration of unknownArrays) {
-  const typeName = typeAlias(declaration.id);
-  console.log(
-    `  type ${typeName}, derived from ${declaration.file}:${declaration.context}`,
-  );
-}
-
-console.log(
-  `
-These type aliases have been given the type "unknown[]". Opening
-${COMMON_FILE}.d.ts and manually providing the proper types is recommended.
-`.trim(),
-);
